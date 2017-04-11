5c00bfdd4e6a54cfcc1fe17759d2ef89345b6247
Olli Nissinen <olli.nissine@gmail.com>
2017-04-11 21:21:30 +0300

    Database connection
    Database connection with test

---
+++ solution/src/main/java/Database/DbManager.java
@@ -0,0 +1,23 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbManager {
+	
+	public Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://138.68.89.66:3306/paivyri", "paivyri", "passu tähän");
+			return conn;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
